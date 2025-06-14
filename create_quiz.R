3c1412dc941a8dd62197e474cb78ec117b9e31e0
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-06-14 15:58:31 +0200

    Reimplement dec_to_rom() (much shorter)

--- create_quiz.R
+++ create_quiz.R
@@ -1,61 +1,11 @@
 #install.packages("xaringan")
-if (FALSE) {
-romans <- c("I", "V", "X", "L", "C", "D", "M", "-", "-")
-
-lmu <- sapply(2*(1:4) - 1, function(i) {
-  paste(romans[i:(i+2)], collapse = "")
-})
-
-a <- 999:1001
-
-n <- length(a)
-ma <- matrix(a, n, 4)
-md <- t(matrix(10^(3:0), 4, n))
-digits <- ma %/% md %% 10
-
-# l = lower, m = middle, u = upper
-pattern <- c(
-    "-",                # 0
-    "l",   "ll", "lll", # 1, 2, 3
-   "lm",    "m",  "ml", # 4, 5, 6
-  "mll", "mlll",  "lu"  # 7, 8, 9
-)
-
-chr <- function(s, i) substr(s, i ,i)
-lookups <- sapply(lmu, function(x) {
-  res <- sapply(pattern, function(p) {
-    p <- gsub("l", chr(x, 1), p)
-    p <- gsub("m", chr(x, 2), p)
-    p <- gsub("u", chr(x, 3), p)
-    p
-  })
-  stats::setNames(res, pattern)
-})
-
-lookups
-dimnames(lookups)
-digits
-p <- matrix(pattern[digits + 1], n)
-p
-
-
-}
 
 file <- "data/jeder-gegen-jeden-fragen.txt"
 
-rom_lookup <- c(
-  I = 1L, 
-  V = 5L,
-  X = 10L,
-  L = 50L, 
-  C = 100L, 
-  D = 500L, 
-  M = 1000L
-)
-
 rom_to_dec <- function(r) {
+  lookup <- c(I = 1L, V = 5L, X = 10L, L = 50L, C = 100L, D = 500L, M = 1000L)
   sapply(strsplit(r, ''), function(x) {
-    values <- rom_lookup[x]
+    values <- lookup[x]
     if (length(i <- which(diff(values) > 0))) {
       values[i] <- values[i+1] - values[i]
       values <- values[-(i+1)]
@@ -65,88 +15,39 @@ rom_to_dec <- function(r) {
 }
 
 dec_to_rom <- function(a) {
-  
-  stopifnot(all(a < 4000L))
-  a <- as.integer(a)
-  
-  if (length(a) > 1L) {
-    return(sapply(a, dec_to_rom))
-  }
-  
-  lookup <- matrix(
-    data = c(
-      1, 0, 0, 0,
-      2, 0, 0, 0,
-      3, 0, 0, 0,
-      1, 1, 0, 0,
-      0, 1, 0, 0,
-      1, 1, 0, 1,
-      2, 1, 0, 1,
-      3, 1, 0, 1,
-      1, 0, 1, 0
-    ),
-    ncol = 4L, 
-    byrow = TRUE, 
-    dimnames = list(NULL, c("lower", "middle", "upper", "swap"))
-  )
-  
-  symbols_for_exponent <- function(e) {
-    symbols <- names(rom_lookup)
-    result <- symbols[seq.int(2*e + 1, min(2*e + 3L, length(symbols)))]
-    names(result) <- c("lower", "middle", "upper")[seq_along(result)]
-    result
-  }
-  
-  collapse <- function(x) paste(x, collapse = "")
-  
-  collapse(sapply(3:0, function(e) {
-    
-    potence <- 10^e
-    times <- a %/% potence
-    
-    if (times == 0L) {
-      return("")
-    }
-    
-    stimes <- if (lookup[times, "swap"]) {
-      lookup[times, 3:1]
-    } else {
-      lookup[times, 1:3]
-    }
-    
-    a <<- a - times * potence
-    collapse(rep(symbols_for_exponent(e)[names(stimes)], stimes))
+  # l = lower, m = middle, u = upper, what letters are used for digits 0:9?
+  template <- c("-", "l", "ll", "lll", "lm", "m", "ml", "mll", "mlll",  "lu")
+  lookup <- do.call(cbind, lapply(c("M--", "CDM", "XLC", "IVX"), function(x) {
+    lmu <- strsplit(x, "")[[1L]]
+    sapply(USE.NAMES = FALSE, template, function(y) {
+      gsub("-", "", gsub("l", lmu[1], gsub("m", lmu[2], gsub("u", lmu[3], y))))
+    })
   }))
+  digits <- matrix(a, length(a), 4) %/% t(matrix(10^(3:0), 4, length(a))) %% 10
+  do.call(paste0, lapply(1:4, function(j) lookup[cbind(digits[, j] + 1, j)]))
 }
 
 stopifnot(identical(rom_to_dec(dec_to_rom(a <- 1:3999)), a))
 
 handle_placeholders <- function(data) {
-  
   rnd <- function(...) {
     sample(do.call(c, list(...)), 1L)
   }
-  
   int <- as.integer
-  
   de_formatted <- function(x, fmt) {
     gsub("\\.", ",", sprintf(fmt, x))
   }
-  
   de_to_num <- function(x) {
     as.numeric(gsub(",", ".", x))
   }
-  
   is_ticked <- function(x) {
     startsWith(x, "`")
   }
-  
   split_into_tokens <- function(x) {
     p <- "\\[+[^]]*\\]+|`[^`]*`|[^][`]*"
     matches <- gregexpr(p, x)
     regmatches(x, matches)
   }
-  
   eval_tokens <- function(tokens) {
     is_expr <- is_ticked(tokens)
     values <- sapply(tokens[is_expr], USE.NAMES = FALSE, function(e) {
@@ -155,12 +56,9 @@ handle_placeholders <- function(data) {
     tokens[is_expr] <- values
     list(tokens = tokens, indices = which(is_expr))
   }
-  
   question_tokens <- split_into_tokens(data$frage)
   answer_tokens <- split_into_tokens(data$antwort)
-  
   indices <- which(sapply(question_tokens, function(x) sum(is_ticked(x)) > 0L))
-  
   for (index in indices) {
     result <- eval_tokens(tokens = question_tokens[[index]])
     question_tokens[[index]] <- result$tokens
@@ -168,10 +66,8 @@ handle_placeholders <- function(data) {
     result <- eval_tokens(answer_tokens[[index]])
     answer_tokens[[index]] <- result$tokens
   }
-  
   data$frage <- sapply(question_tokens, paste, collapse = "")
   data$antwort <- sapply(answer_tokens, paste, collapse = "")
-  
   data
 }
 
